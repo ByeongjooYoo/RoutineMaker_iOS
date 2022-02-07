a5017bc5b91ad90b3d298ab265458497e88670c5
ByeongjooYoo <yoobj17@gmail.com>
2022-02-07 13:48:18 +0900

    [Add] Add Add Event View UI

---
+++ RoutineMaker/RoutineMaker/AddEventViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AddEventViewController.swift
+//  RoutineMaker
+//
+//  Created by ByeongJu Yu on 2022/02/07.
+//
+
+import UIKit
+
+class AddEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func tappedCancelBarButton(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+private extension AddEventViewController {
+
+}
