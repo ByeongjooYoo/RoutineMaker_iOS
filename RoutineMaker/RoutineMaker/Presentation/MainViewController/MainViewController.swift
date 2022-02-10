39451bea92113b0b9621427090564db3fde913fb
ByeongjooYoo <yoobj17@gmail.com>
2022-02-10 14:17:57 +0900

    [Add] EventTableView and EventTableViewCell Adding

--- RoutineMaker/RoutineMaker/Presentation/MainViewController/MainViewController.swift
+++ RoutineMaker/RoutineMaker/Presentation/MainViewController/MainViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet weak var eventTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationController()
+        setupTableView()
     }
 }
 
@@ -20,4 +23,31 @@ private extension MainViewController {
         navigationItem.largeTitleDisplayMode = .always
         navigationController?.navigationBar.prefersLargeTitles = true
     }
+    
+    func setupTableView() {
+        eventTableView.delegate = self
+        eventTableView.dataSource = self
+        let nibName = UINib(nibName: "EventTableViewCell", bundle: nil)
+        eventTableView.register(nibName, forCellReuseIdentifier: "EventTableViewCell")
+
+    }
+}
+
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "EventTableViewCell", for: indexPath) as? EventTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+}
+
+extension MainViewController: UITableViewDelegate {
+    
 }
