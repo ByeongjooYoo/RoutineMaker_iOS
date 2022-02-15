a01e41367a36a3a7befab422e32c7a4e75f3b98b
ByeongjooYoo <yoobj17@gmail.com>
2022-02-15 16:10:17 +0900

    [Add] Week achievement View

--- RoutineMaker/RoutineMaker/Presentation/AchievmentViewController/AchievmentViewController.swift
+++ RoutineMaker/RoutineMaker/Presentation/AchievmentViewController/AchievmentViewController.swift
@@ -6,15 +6,21 @@
 //
 
 import UIKit
+import Charts
 
 class AchievmentViewController: UIViewController {
     @IBOutlet weak var dayView: UIView!
     @IBOutlet weak var dayAchivmentProgressView: UIProgressView!
     
+    @IBOutlet weak var weekBarChartView: BarChartView!
+    let weeks: [String] = ["일", "월", "화", "수", "목", "금", "토"]
+    let completionCount: [Double] = [5, 4, 2, 5, 7, 8, 9]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationController()
         setupDayViewLayout()
+        setupWeekBarChartView()
     }
 }
 
@@ -27,5 +33,39 @@ private extension AchievmentViewController {
     func setupDayViewLayout() {
         dayView.layer.cornerRadius = 10
         dayAchivmentProgressView.layer.cornerRadius = 8
+    }
+    
+    func setupWeekBarChartView() {
+        weekBarChartView.noDataText = "데이터가 없습니다."
+        weekBarChartView.noDataFont = .systemFont(ofSize: 20)
+        weekBarChartView.noDataTextColor = .lightGray
+        
+        var dataEntries: [BarChartDataEntry] = []
+        for i in 0 ..< weeks.count {
+            let dataEntry = BarChartDataEntry(x: Double(i), y: completionCount[i])
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(entries: dataEntries, label: "")
+        
+        chartDataSet.colors = [.systemGreen]
+        chartDataSet.highlightEnabled = false
+        
+        let chartData = BarChartData(dataSet: chartDataSet)
+        chartData.setDrawValues(false)
+        chartData.barWidth = Double(0.5)
+        
+        weekBarChartView.data = chartData
+        weekBarChartView.rightAxis.enabled = false
+        weekBarChartView.doubleTapToZoomEnabled = false
+        weekBarChartView.xAxis.labelPosition = .bottom
+        weekBarChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: weeks)
+        
+        weekBarChartView.xAxis.drawGridLinesEnabled = false
+        weekBarChartView.legend.setCustom(entries: [])
+        
+        weekBarChartView.leftAxis.drawGridLinesEnabled = false
+        weekBarChartView.backgroundColor = .systemBackground
+        weekBarChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
     }
 }
